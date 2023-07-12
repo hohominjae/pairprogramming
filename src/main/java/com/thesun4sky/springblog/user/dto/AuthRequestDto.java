2b62447a0f327a9a8e333bb4724ca77e3613499c
hohominjae <jmj0531@naver.com>
2023-07-12 21:41:22 +0900

    ADMIN_TOKEN받아오는거 1차 발전

--- src/main/java/com/thesun4sky/springblog/user/dto/AuthRequestDto.java
+++ src/main/java/com/thesun4sky/springblog/user/dto/AuthRequestDto.java
@@ -8,16 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthRequestDto {
-    @Pattern(regexp = "^[a-z0-9]{4,10}$",
-             message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9) 로 구성되어야 합니다.")
+//    @Pattern(regexp = "^[a-z0-9]{4,10}$",
+//             message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9) 로 구성되어야 합니다.")
     private String username;
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9]{8,15}$",
-             message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9) 로 구성되어야 합니다.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9]{8,15}$",
+//             message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9) 로 구성되어야 합니다.")
     private String password;
 
-    @Pattern(regexp = "ADMIN|USER",
-             message = "ADMIN 혹은 USER만 입력해주세요")
+    @Pattern(regexp = "ADMIN|USER")
+//             message = "ADMIN 혹은 USER만 입력해주세요")
     private UserRoleEnum role;
     private boolean admin = false;
     private String adminToken = "";
