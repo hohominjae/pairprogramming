4532f86287b2571440e99c3ef0163742c0068ddc
hohominjae <jmj0531@naver.com>
2023-07-13 00:57:26 +0900

    ADMIN 구현 완료!!!!!!!!!!!!!!

--- src/main/java/com/thesun4sky/springblog/user/entity/User.java
+++ src/main/java/com/thesun4sky/springblog/user/entity/User.java
@@ -25,7 +25,6 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-
     public User(String username, String password, UserRoleEnum role) {
         this.username = username;
         this.password = password;
